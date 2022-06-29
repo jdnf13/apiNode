7c80379a2b2fd7c0e771a522d0870cb989137fb0
jdnf13 <jdnf13@gmail.com>
2022-06-28 20:18:43 -0500

    ultimos cambios en servicios

--- src/models/portafolio.js
+++ src/models/portafolio.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose');
 const Schema  = mongoose.Schema;
 
 const dataSchema = new Schema({
+    _id:Number,
     producto: String,
     valor: String   
 });
--- src/rutas/routes.js
+++ src/rutas/routes.js
@@ -1,18 +1,81 @@
 const {Router} = require('express');
 const router = Router();
 const Portafolio  = require('../models/portafolio');
-//const _ = require('underscore');
 //rutas de acceso
-router.get('/', async (req,res) => {
-    //console.log('llamado a ruta');
+router.get('/productos-get', async (req,res) => {
     try {
         console.log('intentando obtener data',Portafolio);
         const arrayPortafolio = await Portafolio.find()
         const data = {"data":arrayPortafolio};
         res.json(data);
-        //console.log(data);
     } catch (error) {
         console.log('Error obteniendo data',error);
     }    
+});
+router.post('/insert', async (req,res) => {
+    console.log('recibiendo data ',req.body);
+    const {producto,valor} = req.body;
+    const arrayPortafolio = await Portafolio.find();
+    const _id = arrayPortafolio.length + 1; 
+    const exist = await Portafolio.findOne({_id});
+    const exist_producto = await Portafolio.findOne({producto});
+
+    if(exist || exist_producto){
+        res.status(500).json({"mensaje":"el registro ya fue almacenado anteriormente"});
+    }else{
+        try {
+            Portafolio.insertMany([
+                {
+                    _id:_id,
+                    "producto":producto,
+                    "valor":valor
+                },
+            ],{w:"majority",wtimeout:100});
+            res.status(200).json({"mensaje":"registro almacenado"});
+        } catch (error) {
+            console.log('Error obteniendo data',error);
+        }  
+    }
+});
+router.delete('/delete', async (req,res) => {
+    console.log('recibiendo data ',req.body);
+    const {_id} = req.body;
+    const exist = await Portafolio.findOne({_id});
+
+    if(exist){
+        try {
+            Portafolio.deleteOne({_id:_id});
+            res.json({"mensaje":"registro eliminado"});
+        } catch (error) {
+            console.log('Error obteniendo data',error);
+        }
+    }else{
+        res.json({"mensaje":"registro no existe"});
+    }
+});
+router.put('/actualizar', async (req,res) => {
+    try {
+        
+        const {_id,producto,valor} = req.body;
+        if(producto && valor && _id){
+            const exist = await Portafolio.findOne({producto});
+            if(exist && exist._id !== _id){
+                return res.status(500).json({"mensaje":"el registro no existe"});
+            }
+            const insert = await Portafolio.update(
+                {_id:_id},
+                {
+                    $set: {
+                    producto: producto,
+                    valor: valor
+                    }
+                });
+            return res.status(200).json({"mensaje":"el registro se actualizo","data":insert});
+        }
+        return res.status(500).json({"mensaje":"datos requeridos"});
+
+    } catch (error) {
+        return res.status(500).json({"mensaje":"Error interno"});
+    }
 });
 module.exports = router
